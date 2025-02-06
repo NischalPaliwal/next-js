e7bca9611598fdb78fc4b238edcb1302ae3d612f
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-06 18:42:15 +0530

    Add button click handler to sign-in page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 export default function Signin() {
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -11,7 +12,7 @@ export default function Signin() {
                     <div className="pt-2">
                         <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                        <button onClick={buttonHandler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
                 </div>
             </a>
@@ -30,4 +31,8 @@ function LabelledInput({ label, placeholder, type }: LabelledInputType) {
         <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
         <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
     </div>
+}
+
+const buttonHandler = () => {
+    console.log("Button clicked");
 }
